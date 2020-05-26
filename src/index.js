0f0fa8d644d5ac23c58d279068480608373acd4a
Fabian Hinz <hinzfabian.fh@gmail.com>
2020-05-26 22:44:33 +0200

    dynamic context creation

--- src/index.js
+++ src/index.js
@@ -1,58 +1,66 @@
-import React, { useState, useContext, createContext } from 'react';
+import React, { useState, useContext, createContext, useMemo, useEffect } from 'react';
 import { render } from 'react-dom';
 import './index.css'
 
-const ContextOne = createContext(null)
-const ProviderOne = props => {
-  const [value, setValue] = useState(new Date().getTime())
-  setTimeout(() => setValue(new Date().getTime()), 100)
-  return <ContextOne.Provider value={{ value, setValue }}>{props.children}</ContextOne.Provider>
+const createContexts = (ms, index, numberOfContexts) => {
+    const Context = createContext(null)
+
+    const Provider = props => {
+        const [value, setValue] = useState(0)
+
+        useEffect(() => {
+            const timeout = setTimeout(() => {
+                if (index === 0) console.time(`duration (${numberOfContexts})`)
+                if (index === numberOfContexts - 1) console.timeEnd(`duration (${numberOfContexts})`)
+                setValue(value + 1)
+            }, ms)
+
+            return () => {
+                clearTimeout(timeout)
+            }
+        }, [value])
+
+        return <Context.Provider value={{ value }}>{props.children}</Context.Provider>
+    }
+
+    const Consumer = () => {
+        const { value } = useContext(Context)
+
+        return <div className="consumer">{value}</div>
+    }
+
+    return { Provider, Consumer }
 }
 
-const ContextTwo = createContext(null)
-const ProviderTwo = props => {
-  const [value] = useState(new Date().getTime())
-  return <ContextTwo.Provider value={{ value }}>{props.children}</ContextTwo.Provider>
+const App = () => {
+    const [numberOfContexts, setNumberOfContexts] = useState(250)
+    const [ms, setMs] = useState(1000)
+
+    const memoContexts = useMemo(() => (
+        new Array(numberOfContexts).fill(1).map((_, index) => 
+            createContexts(ms, index, numberOfContexts)
+        )
+    ), [ms, numberOfContexts])
+
+    return (
+        <>
+            <input type="number" value={numberOfContexts} onChange={e =>
+                setNumberOfContexts(e.target.value)
+            } />
+
+            <input type="number" value={ms} onChange={e =>
+                setMs(e.target.value)
+            } />
+
+            <div className="providers">
+                {memoContexts.map(({ Provider, Consumer }, index) =>
+                    <Provider key={index}>
+                        <Consumer />
+                    </Provider>
+                )}
+            </div>
+        </>
+    )
 }
-
-const AppProvider = props => (
-  <ProviderOne>
-    <ProviderTwo>
-      {props.children}
-    </ProviderTwo>
-  </ProviderOne>
-)
-
-const ContextOneConsumer = () => {
-  const { value } = useContext(ContextOne)
-  console.log("render of One")
-  return <h2>{value}</h2>
-}
-
-const ContextTwoConsumer = () => {
-  const { value } = useContext(ContextTwo)
-  console.log("render of Two")
-  return <h2>{value}</h2>
-}
-
-const ManInTheMiddle = props => {
-  console.log("render of ManInTheMiddle")
-  return (
-    <>
-      <h1>Oh hi there</h1>
-      <h1>Hello again</h1>
-      <div className="consumers">{props.children}</div>
-    </>
-  )
-}
-
-const App = () => (
-  <AppProvider>
-    <ManInTheMiddle>
-      <ContextOneConsumer />
-      <ContextTwoConsumer />
-    </ManInTheMiddle>
-  </AppProvider>
-)
 
 render(<App />, document.getElementById('root'));
